788d8692fc99457eb0e5a20145fecc643b261985
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:18:22 -0500

    suma

--- nachol.java
+++ nachol.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class nachol {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("vamos a sumar");
+        int n1 = 0, n2 = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese el primer numero");
+        n1 = sc.nextInt();
+        System.out.println("ingrese el segundo numero");
+        n2 = sc.nextInt();
+        System.out.println("la suma de los numero es" +(n1+n2));
+
     }
 }
